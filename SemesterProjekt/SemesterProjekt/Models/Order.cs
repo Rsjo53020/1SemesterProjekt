17da6a2c7988eb4f2bc6fc245fe929f6d2f02df9
RS\ronni <rsjo53020@edu.ucl.dk>
2023-05-23 12:20:08 +0200

    opsætning af OPG

---
+++ SemesterProjekt/SemesterProjekt/Models/Order.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SemesterProjekt.Models
+{
+    internal class Order
+    {
+        private string CostumerID { get; set; }
+        private int ProductLine { get; set; }
+        private DateTime OrderDate { get; set; }
+        private Decimal TotalPrice { get; set; }
+
+        public void FindOrder(Models.Customer customer)
+        {
+            return customer;
+        }
+
+        public void PlaceOrder()
+        {
+
+        }
+
+        public void DeleteOrder()
+        {
+
+        }
+
+    }
+}
---
+++ SemesterProjekt/SemesterProjekt/Models/StoreStock.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SemesterProjekt.Models
+{
+    public class StoreStock
+    {
+
+    }
+
+    public static string ViewProductInfo()
+    {
+        return "";
+    }
+}
