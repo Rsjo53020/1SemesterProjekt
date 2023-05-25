f99d4ce6536ccdbe882dd5a5d377efe78dd8d780
CHRISTOFFER\chris <chsk52994@edu.ucl.dk>
2023-05-25 11:05:39 +0200

    Fundet ordre

---
+++ SemesterProjekt GUI/SemesterProjekt/GUI/FundetOrdre.Designer.cs
@@ -0,0 +1,255 @@
+﻿namespace SemesterProjekt.GUI
+{
+    partial class FundetOrdre
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.PANEL_FindOrder = new System.Windows.Forms.Panel();
+            this.LB_FoundOrder = new System.Windows.Forms.Label();
+            this.dateTimePicker1 = new System.Windows.Forms.DateTimePicker();
+            this.TB_LineID = new System.Windows.Forms.TextBox();
+            this.LB_LineID = new System.Windows.Forms.Label();
+            this.TB_CustomerID = new System.Windows.Forms.TextBox();
+            this.LB_CustomerID = new System.Windows.Forms.Label();
+            this.TB_VAT = new System.Windows.Forms.TextBox();
+            this.LB_VAT = new System.Windows.Forms.Label();
+            this.TB_TotalPrice = new System.Windows.Forms.TextBox();
+            this.LB_TotalPrice = new System.Windows.Forms.Label();
+            this.TB_OrderStatus = new System.Windows.Forms.TextBox();
+            this.LB_OrderStatus = new System.Windows.Forms.Label();
+            this.LB_OrderDate = new System.Windows.Forms.Label();
+            this.TB_OrderID = new System.Windows.Forms.TextBox();
+            this.LB_OrderID = new System.Windows.Forms.Label();
+            this.PANEL_FindOrder.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // PANEL_FindOrder
+            // 
+            this.PANEL_FindOrder.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(35)))), ((int)(((byte)(32)))), ((int)(((byte)(39)))));
+            this.PANEL_FindOrder.Controls.Add(this.LB_FoundOrder);
+            this.PANEL_FindOrder.Dock = System.Windows.Forms.DockStyle.Top;
+            this.PANEL_FindOrder.Location = new System.Drawing.Point(0, 0);
+            this.PANEL_FindOrder.Name = "PANEL_FindOrder";
+            this.PANEL_FindOrder.Size = new System.Drawing.Size(1426, 100);
+            this.PANEL_FindOrder.TabIndex = 30;
+            // 
+            // LB_FoundOrder
+            // 
+            this.LB_FoundOrder.AutoSize = true;
+            this.LB_FoundOrder.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F);
+            this.LB_FoundOrder.ForeColor = System.Drawing.Color.Gainsboro;
+            this.LB_FoundOrder.Location = new System.Drawing.Point(44, 28);
+            this.LB_FoundOrder.Name = "LB_FoundOrder";
+            this.LB_FoundOrder.Size = new System.Drawing.Size(208, 37);
+            this.LB_FoundOrder.TabIndex = 16;
+            this.LB_FoundOrder.Text = "Fundet Ordre";
+            // 
+            // dateTimePicker1
+            // 
+            this.dateTimePicker1.CalendarFont = new System.Drawing.Font("Microsoft Sans Serif", 15.75F);
+            this.dateTimePicker1.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F);
+            this.dateTimePicker1.Location = new System.Drawing.Point(272, 216);
+            this.dateTimePicker1.Name = "dateTimePicker1";
+            this.dateTimePicker1.Size = new System.Drawing.Size(318, 43);
+            this.dateTimePicker1.TabIndex = 29;
+            // 
+            // TB_LineID
+            // 
+            this.TB_LineID.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F);
+            this.TB_LineID.Location = new System.Drawing.Point(708, 133);
+            this.TB_LineID.Multiline = true;
+            this.TB_LineID.Name = "TB_LineID";
+            this.TB_LineID.Size = new System.Drawing.Size(577, 426);
+            this.TB_LineID.TabIndex = 28;
+            // 
+            // LB_LineID
+            // 
+            this.LB_LineID.AutoSize = true;
+            this.LB_LineID.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F);
+            this.LB_LineID.ForeColor = System.Drawing.Color.Gainsboro;
+            this.LB_LineID.Location = new System.Drawing.Point(493, 139);
+            this.LB_LineID.Name = "LB_LineID";
+            this.LB_LineID.Size = new System.Drawing.Size(165, 37);
+            this.LB_LineID.TabIndex = 27;
+            this.LB_LineID.Text = "Produkter:";
+            // 
+            // TB_CustomerID
+            // 
+            this.TB_CustomerID.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F);
+            this.TB_CustomerID.Location = new System.Drawing.Point(272, 291);
+            this.TB_CustomerID.Name = "TB_CustomerID";
+            this.TB_CustomerID.Size = new System.Drawing.Size(386, 43);
+            this.TB_CustomerID.TabIndex = 26;
+            // 
+            // LB_CustomerID
+            // 
+            this.LB_CustomerID.AutoSize = true;
+            this.LB_CustomerID.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F);
+            this.LB_CustomerID.ForeColor = System.Drawing.Color.Gainsboro;
+            this.LB_CustomerID.Location = new System.Drawing.Point(57, 297);
+            this.LB_CustomerID.Name = "LB_CustomerID";
+            this.LB_CustomerID.Size = new System.Drawing.Size(118, 37);
+            this.LB_CustomerID.TabIndex = 25;
+            this.LB_CustomerID.Text = "Kunde:";
+            // 
+            // TB_VAT
+            // 
+            this.TB_VAT.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F);
+            this.TB_VAT.Location = new System.Drawing.Point(272, 372);
+            this.TB_VAT.Name = "TB_VAT";
+            this.TB_VAT.Size = new System.Drawing.Size(260, 43);
+            this.TB_VAT.TabIndex = 24;
+            // 
+            // LB_VAT
+            // 
+            this.LB_VAT.AutoSize = true;
+            this.LB_VAT.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F);
+            this.LB_VAT.ForeColor = System.Drawing.Color.Gainsboro;
+            this.LB_VAT.Location = new System.Drawing.Point(57, 378);
+            this.LB_VAT.Name = "LB_VAT";
+            this.LB_VAT.Size = new System.Drawing.Size(97, 37);
+            this.LB_VAT.TabIndex = 23;
+            this.LB_VAT.Text = "Butik:";
+            // 
+            // TB_TotalPrice
+            // 
+            this.TB_TotalPrice.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F);
+            this.TB_TotalPrice.Location = new System.Drawing.Point(1025, 600);
+            this.TB_TotalPrice.Name = "TB_TotalPrice";
+            this.TB_TotalPrice.Size = new System.Drawing.Size(260, 43);
+            this.TB_TotalPrice.TabIndex = 22;
+            // 
+            // LB_TotalPrice
+            // 
+            this.LB_TotalPrice.AutoSize = true;
+            this.LB_TotalPrice.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F);
+            this.LB_TotalPrice.ForeColor = System.Drawing.Color.Gainsboro;
+            this.LB_TotalPrice.Location = new System.Drawing.Point(810, 606);
+            this.LB_TotalPrice.Name = "LB_TotalPrice";
+            this.LB_TotalPrice.Size = new System.Drawing.Size(162, 37);
+            this.LB_TotalPrice.TabIndex = 21;
+            this.LB_TotalPrice.Text = "Pris Total:";
+            // 
+            // TB_OrderStatus
+            // 
+            this.TB_OrderStatus.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F);
+            this.TB_OrderStatus.Location = new System.Drawing.Point(272, 449);
+            this.TB_OrderStatus.Name = "TB_OrderStatus";
+            this.TB_OrderStatus.Size = new System.Drawing.Size(153, 43);
+            this.TB_OrderStatus.TabIndex = 20;
+            // 
+            // LB_OrderStatus
+            // 
+            this.LB_OrderStatus.AutoSize = true;
+            this.LB_OrderStatus.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F);
+            this.LB_OrderStatus.ForeColor = System.Drawing.Color.Gainsboro;
+            this.LB_OrderStatus.Location = new System.Drawing.Point(57, 455);
+            this.LB_OrderStatus.Name = "LB_OrderStatus";
+            this.LB_OrderStatus.Size = new System.Drawing.Size(208, 37);
+            this.LB_OrderStatus.TabIndex = 19;
+            this.LB_OrderStatus.Text = "Ordre Status:";
+            // 
+            // LB_OrderDate
+            // 
+            this.LB_OrderDate.AutoSize = true;
+            this.LB_OrderDate.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F);
+            this.LB_OrderDate.ForeColor = System.Drawing.Color.Gainsboro;
+            this.LB_OrderDate.Location = new System.Drawing.Point(57, 222);
+            this.LB_OrderDate.Name = "LB_OrderDate";
+            this.LB_OrderDate.Size = new System.Drawing.Size(185, 37);
+            this.LB_OrderDate.TabIndex = 18;
+            this.LB_OrderDate.Text = "Ordre Dato:";
+            // 
+            // TB_OrderID
+            // 
+            this.TB_OrderID.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F);
+            this.TB_OrderID.Location = new System.Drawing.Point(272, 139);
+            this.TB_OrderID.Name = "TB_OrderID";
+            this.TB_OrderID.Size = new System.Drawing.Size(153, 43);
+            this.TB_OrderID.TabIndex = 17;
+            // 
+            // LB_OrderID
+            // 
+            this.LB_OrderID.AutoSize = true;
+            this.LB_OrderID.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F);
+            this.LB_OrderID.ForeColor = System.Drawing.Color.Gainsboro;
+            this.LB_OrderID.Location = new System.Drawing.Point(57, 145);
+            this.LB_OrderID.Name = "LB_OrderID";
+            this.LB_OrderID.Size = new System.Drawing.Size(148, 37);
+            this.LB_OrderID.TabIndex = 16;
+            this.LB_OrderID.Text = "Ordre ID:";
+            // 
+            // FundetOrdre
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(35)))), ((int)(((byte)(51)))), ((int)(((byte)(76)))));
+            this.ClientSize = new System.Drawing.Size(1426, 695);
+            this.Controls.Add(this.PANEL_FindOrder);
+            this.Controls.Add(this.dateTimePicker1);
+            this.Controls.Add(this.TB_LineID);
+            this.Controls.Add(this.LB_LineID);
+            this.Controls.Add(this.TB_CustomerID);
+            this.Controls.Add(this.LB_CustomerID);
+            this.Controls.Add(this.TB_VAT);
+            this.Controls.Add(this.LB_VAT);
+            this.Controls.Add(this.TB_TotalPrice);
+            this.Controls.Add(this.LB_TotalPrice);
+            this.Controls.Add(this.TB_OrderStatus);
+            this.Controls.Add(this.LB_OrderStatus);
+            this.Controls.Add(this.LB_OrderDate);
+            this.Controls.Add(this.TB_OrderID);
+            this.Controls.Add(this.LB_OrderID);
+            this.Name = "FundetOrdre";
+            this.Text = "FundetOrdre";
+            this.PANEL_FindOrder.ResumeLayout(false);
+            this.PANEL_FindOrder.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel PANEL_FindOrder;
+        private System.Windows.Forms.Label LB_FoundOrder;
+        private System.Windows.Forms.DateTimePicker dateTimePicker1;
+        private System.Windows.Forms.TextBox TB_LineID;
+        private System.Windows.Forms.Label LB_LineID;
+        private System.Windows.Forms.TextBox TB_CustomerID;
+        private System.Windows.Forms.Label LB_CustomerID;
+        private System.Windows.Forms.TextBox TB_VAT;
+        private System.Windows.Forms.Label LB_VAT;
+        private System.Windows.Forms.TextBox TB_TotalPrice;
+        private System.Windows.Forms.Label LB_TotalPrice;
+        private System.Windows.Forms.TextBox TB_OrderStatus;
+        private System.Windows.Forms.Label LB_OrderStatus;
+        private System.Windows.Forms.Label LB_OrderDate;
+        private System.Windows.Forms.TextBox TB_OrderID;
+        private System.Windows.Forms.Label LB_OrderID;
+    }
+}
