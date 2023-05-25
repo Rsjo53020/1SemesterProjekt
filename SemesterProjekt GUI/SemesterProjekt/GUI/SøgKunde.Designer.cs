9e066c0d43e64f2dc6cdcb65fbfbcc9c1aba1086
CHRISTOFFER\chris <chsk52994@edu.ucl.dk>
2023-05-25 14:28:04 +0200

    dock

--- SemesterProjekt GUI/SemesterProjekt/GUI/SøgKunde.Designer.cs
+++ SemesterProjekt GUI/SemesterProjekt/GUI/SøgKunde.Designer.cs
@@ -46,8 +46,9 @@
             this.panel1.Controls.Add(this.LB_FindOrder);
             this.panel1.Dock = System.Windows.Forms.DockStyle.Top;
             this.panel1.Location = new System.Drawing.Point(0, 0);
+            this.panel1.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
             this.panel1.Name = "panel1";
-            this.panel1.Size = new System.Drawing.Size(800, 62);
+            this.panel1.Size = new System.Drawing.Size(900, 78);
             this.panel1.TabIndex = 0;
             // 
             // LB_FindOrder
@@ -55,9 +56,9 @@
             this.LB_FindOrder.AutoSize = true;
             this.LB_FindOrder.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F);
             this.LB_FindOrder.ForeColor = System.Drawing.Color.Gainsboro;
-            this.LB_FindOrder.Location = new System.Drawing.Point(331, 16);
+            this.LB_FindOrder.Location = new System.Drawing.Point(372, 20);
             this.LB_FindOrder.Name = "LB_FindOrder";
-            this.LB_FindOrder.Size = new System.Drawing.Size(147, 31);
+            this.LB_FindOrder.Size = new System.Drawing.Size(175, 37);
             this.LB_FindOrder.TabIndex = 17;
             this.LB_FindOrder.Text = "Søg Kunde";
             // 
@@ -66,9 +67,9 @@
             this.label1.AutoSize = true;
             this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F);
             this.label1.ForeColor = System.Drawing.Color.Gainsboro;
-            this.label1.Location = new System.Drawing.Point(56, 100);
+            this.label1.Location = new System.Drawing.Point(63, 125);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(205, 31);
+            this.label1.Size = new System.Drawing.Size(245, 37);
             this.label1.TabIndex = 18;
             this.label1.Text = "Kunde Tlf + 45 :";
             // 
@@ -77,54 +78,59 @@
             this.label2.AutoSize = true;
             this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F);
             this.label2.ForeColor = System.Drawing.Color.Gainsboro;
-            this.label2.Location = new System.Drawing.Point(56, 150);
+            this.label2.Location = new System.Drawing.Point(63, 188);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(190, 31);
+            this.label2.Size = new System.Drawing.Size(225, 37);
             this.label2.TabIndex = 19;
             this.label2.Text = "Kunde E-Mail :";
             // 
             // TB_Tlf
             // 
-            this.TB_Tlf.Location = new System.Drawing.Point(267, 109);
+            this.TB_Tlf.Location = new System.Drawing.Point(300, 136);
+            this.TB_Tlf.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
             this.TB_Tlf.Name = "TB_Tlf";
-            this.TB_Tlf.Size = new System.Drawing.Size(325, 22);
+            this.TB_Tlf.Size = new System.Drawing.Size(365, 26);
             this.TB_Tlf.TabIndex = 20;
             // 
             // TB_EMail
             // 
-            this.TB_EMail.Location = new System.Drawing.Point(267, 150);
+            this.TB_EMail.Location = new System.Drawing.Point(300, 188);
+            this.TB_EMail.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
             this.TB_EMail.Name = "TB_EMail";
-            this.TB_EMail.Size = new System.Drawing.Size(325, 22);
+            this.TB_EMail.Size = new System.Drawing.Size(365, 26);
             this.TB_EMail.TabIndex = 21;
             // 
             // dataGridView1_SearchCustomer
             // 
             this.dataGridView1_SearchCustomer.BackgroundColor = System.Drawing.SystemColors.ActiveBorder;
             this.dataGridView1_SearchCustomer.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            this.dataGridView1_SearchCustomer.Location = new System.Drawing.Point(12, 197);
+            this.dataGridView1_SearchCustomer.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.dataGridView1_SearchCustomer.Location = new System.Drawing.Point(0, 244);
+            this.dataGridView1_SearchCustomer.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
             this.dataGridView1_SearchCustomer.Name = "dataGridView1_SearchCustomer";
             this.dataGridView1_SearchCustomer.RowHeadersWidth = 51;
             this.dataGridView1_SearchCustomer.RowTemplate.Height = 24;
-            this.dataGridView1_SearchCustomer.Size = new System.Drawing.Size(776, 241);
+            this.dataGridView1_SearchCustomer.Size = new System.Drawing.Size(900, 318);
             this.dataGridView1_SearchCustomer.TabIndex = 22;
             // 
             // BTN_SearchCustomer
             // 
             this.BTN_SearchCustomer.BackColor = System.Drawing.Color.White;
             this.BTN_SearchCustomer.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
-            this.BTN_SearchCustomer.Location = new System.Drawing.Point(624, 109);
+            this.BTN_SearchCustomer.Location = new System.Drawing.Point(702, 136);
+            this.BTN_SearchCustomer.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
             this.BTN_SearchCustomer.Name = "BTN_SearchCustomer";
-            this.BTN_SearchCustomer.Size = new System.Drawing.Size(164, 63);
+            this.BTN_SearchCustomer.Size = new System.Drawing.Size(184, 79);
             this.BTN_SearchCustomer.TabIndex = 23;
             this.BTN_SearchCustomer.Text = "Søg Kunde";
             this.BTN_SearchCustomer.UseVisualStyleBackColor = false;
             // 
             // SøgKunde
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(35)))), ((int)(((byte)(51)))), ((int)(((byte)(76)))));
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(900, 562);
             this.Controls.Add(this.BTN_SearchCustomer);
             this.Controls.Add(this.dataGridView1_SearchCustomer);
             this.Controls.Add(this.TB_EMail);
@@ -132,6 +138,7 @@
             this.Controls.Add(this.label2);
             this.Controls.Add(this.label1);
             this.Controls.Add(this.panel1);
+            this.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
             this.Name = "SøgKunde";
             this.Text = "SøgKunde";
             this.panel1.ResumeLayout(false);
