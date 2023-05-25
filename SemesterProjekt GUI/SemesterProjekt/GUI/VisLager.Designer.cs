63c1b9f4c3c4dc6d76502f9c0f00d365e474aeec
RS\ronni <rsjo53020@edu.ucl.dk>
2023-05-25 14:41:36 +0200

    Test

---
+++ SemesterProjekt GUI/SemesterProjekt/GUI/VisLager.Designer.cs
@@ -0,0 +1,93 @@
+﻿namespace SemesterProjekt.GUI
+{
+    partial class VisLager
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.PANEL_FindOrder = new System.Windows.Forms.Panel();
+            this.LB_ShowStock = new System.Windows.Forms.Label();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.PANEL_FindOrder.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // PANEL_FindOrder
+            // 
+            this.PANEL_FindOrder.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(35)))), ((int)(((byte)(32)))), ((int)(((byte)(39)))));
+            this.PANEL_FindOrder.Controls.Add(this.LB_ShowStock);
+            this.PANEL_FindOrder.Dock = System.Windows.Forms.DockStyle.Top;
+            this.PANEL_FindOrder.Location = new System.Drawing.Point(0, 0);
+            this.PANEL_FindOrder.Name = "PANEL_FindOrder";
+            this.PANEL_FindOrder.Size = new System.Drawing.Size(1238, 100);
+            this.PANEL_FindOrder.TabIndex = 31;
+            // 
+            // LB_ShowStock
+            // 
+            this.LB_ShowStock.AutoSize = true;
+            this.LB_ShowStock.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F);
+            this.LB_ShowStock.ForeColor = System.Drawing.Color.Gainsboro;
+            this.LB_ShowStock.Location = new System.Drawing.Point(44, 28);
+            this.LB_ShowStock.Name = "LB_ShowStock";
+            this.LB_ShowStock.Size = new System.Drawing.Size(153, 37);
+            this.LB_ShowStock.TabIndex = 16;
+            this.LB_ShowStock.Text = "Vis Lager";
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.dataGridView1.Location = new System.Drawing.Point(0, 100);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.RowHeadersWidth = 62;
+            this.dataGridView1.RowTemplate.Height = 28;
+            this.dataGridView1.Size = new System.Drawing.Size(1238, 676);
+            this.dataGridView1.TabIndex = 32;
+            // 
+            // VisLager
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(35)))), ((int)(((byte)(51)))), ((int)(((byte)(76)))));
+            this.ClientSize = new System.Drawing.Size(1238, 776);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.PANEL_FindOrder);
+            this.Name = "VisLager";
+            this.Text = "VisLager";
+            this.PANEL_FindOrder.ResumeLayout(false);
+            this.PANEL_FindOrder.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel PANEL_FindOrder;
+        private System.Windows.Forms.Label LB_ShowStock;
+        private System.Windows.Forms.DataGridView dataGridView1;
+    }
+}
