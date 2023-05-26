0c32de6c15f81ff4afde479ec9a02ed8bff929cf
Alexander DESKTOP-85QQ2VK <alexander.c.larsen@gmail.com>
2023-05-26 09:17:10 +0200

    Program.cs

--- SemesterProjekt GUI/SemesterProjekt/Program.cs
+++ SemesterProjekt GUI/SemesterProjekt/Program.cs
@@ -3,10 +3,15 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows.Forms;
 
 namespace SemesterProjekt
 {
     internal class Program
     {
+        static void Main(string[] args)
+        {
+            Application.Run(new GUIMain());
+        }
     }
 }
