e071621b3dd20d94a233d9725d2937d64e14954f
RS\ronni <rsjo53020@edu.ucl.dk>
2023-05-26 11:20:03 +0200

    get customer

--- SemesterProjekt GUI/SemesterProjekt/Models/Database.cs
+++ SemesterProjekt GUI/SemesterProjekt/Models/Database.cs
@@ -18,22 +18,47 @@ namespace SemesterProjekt.Models
         public static string strconn = "Data Source=mssql4.unoeuro.com;Initial Catalog=cskafte_dk_db_skafte;User ID=cskafte_dk;Password=3tfep5Gc4wgAzxDH2rEy";
 
         /// <summary>
-        /// (C)RUD; FirstName (string), SurName(string), PhoneNr(string), EMailAdress(string), Adress(string),
+        /// (C)RUD on Customer: FirstName (string), SurName(string), PhoneNr(string), EMailAdress(string), Adress(string),
         /// City(string), PostalCode(string),
         /// Discount(Desimal), Birthday(DateTime(YYYY-MM-DD)), Age(int), VisionTest(string)
         /// </summary>
-        public static void SqlCreateCustumer(Models.Customer customer)       
+        public static void SqlCreateCustumer(Models.Customer customer)
         {
-            //kalder forbindelsen på simly.com
+            //call connection to database
             SqlConnection conn = new SqlConnection(strconn);
 
-           
-            string SQL = $"INSERT INTO Customer Values ({customer.})
+            string sSQL = $"INSERT INTO Customer Values ({customer.FirstName}, {customer.SurName}, " +
+                $"{customer.PhoneNr}, {customer.Mail}, {customer.Address}, {customer.City}, " +
+                $"{customer.PostalCode}, {customer.Discount}, {customer.Birthday}, {customer.Age}, {customer.VisionTest});";
+
+
+            SqlCommand command = new SqlCommand(sSQL, conn);
+            conn.Open(); //Open connection to Database 
+            command.ExecuteNonQuery();
+            conn.Close(); //Close connection to Database
         }
 
-
-        public static Customer SqlGetCustomer(string phoneNr, string mail)
+        /// <summary>
+        /// C(R)UD on Customer: takes two parameters to find a customer
+        /// </summary>
+        public static Customer SqlGetCustomer(string phoneNr = "", string mail = "")
         {
+            //call connection to database
+            SqlConnection conn = new SqlConnection(strconn);
+
+            if (mail.Contains("@"))
+            {
+                string sSQL = string sSQL = $"SELECT * FROM Customer WHERE EMailAdress='{mail}';";
+            }
+            else (phoneNr.c)
+            string sSQL = string sSQL = $"SELECT * FROM Customer WHERE EMailAdress='{phoneNr}';";
+
+
+            SqlCommand command = new SqlCommand(sSQL, conn);
+            conn.Open(); //Open connection to Database 
+            command.ExecuteNonQuery();
+            conn.Close(); //Close connection to Database
+
             return Customer;
         }
 
