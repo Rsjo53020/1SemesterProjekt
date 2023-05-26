42f8424f25cff6c3e2d2e7ae7b2d21a6c8eea4ec
RS\ronni <rsjo53020@edu.ucl.dk>
2023-05-26 10:24:23 +0200

    (C)RUD V1

--- SemesterProjekt GUI/SemesterProjekt/Models/Database.cs
+++ SemesterProjekt GUI/SemesterProjekt/Models/Database.cs
@@ -4,28 +4,32 @@ using System.ComponentModel.Design;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Data.SqlClient;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement;
+using System.Net.Mail;
+
 
 namespace SemesterProjekt.Models
 {
     public class Database
-    {
-        public static string ConnectSQL()
-        {
-            string strconn = "Data Source=mssql4.unoeuro.com;Initial Catalog=cskafte_dk_db_skafte;User ID=cskafte_dk;Password=3tfep5Gc4wgAzxDH2rEy";
 
-            return strconn;
-        }
+    {
+
+        public static string strconn = "Data Source=mssql4.unoeuro.com;Initial Catalog=cskafte_dk_db_skafte;User ID=cskafte_dk;Password=3tfep5Gc4wgAzxDH2rEy";
+
         /// <summary>
         /// (C)RUD; FirstName (string), SurName(string), PhoneNr(string), EMailAdress(string), Adress(string),
         /// City(string), PostalCode(string),
         /// Discount(Desimal), Birthday(DateTime(YYYY-MM-DD)), Age(int), VisionTest(string)
         /// </summary>
-        public static void SqlCreateCustumer(Customer customer)
+        public static void SqlCreateCustumer(Models.Customer customer)
         {
-            string SQLstring = "insert into Customer(FirstName, SurName, PhoneNr, EMailAdress, Adress," +
-                " City, PostalCode, Discount, Birthday, Age, VisionTest) " +
-                "Values('jens', 'jensen', '87654321', 'test.jensen@test.test', 'jensenvej 22', 'københavn'," +
-                " '1526', 0.10, '1986-05-12', 37, '+2.5');";
+            //kalder forbindelsen på simly.com
+            SqlConnection conn = new SqlConnection(strconn);
+
+
+            string SQL = $"INSERT INTO Customer Values ({})
+                                        Values('jens', 'jensen', '87654321', 'test.jensen@test.test', 'jensenvej 22', 'københavn', '1526', 0.10, '1986-05-12', 37, '+2.5')";
 
         }
 
@@ -82,6 +86,6 @@ namespace SemesterProjekt.Models
             return Product;
         }
     }
-    
+
 
 }
