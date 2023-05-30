4781584884701d36209db5303299334e788947a8
CHRISTOFFER\chris <chsk52994@edu.ucl.dk>
2023-05-30 14:42:13 +0200

    Create customer ui

--- SemesterProjekt GUI/SemesterProjekt/GUI/OpretKunde.cs
+++ SemesterProjekt GUI/SemesterProjekt/GUI/OpretKunde.cs
@@ -16,10 +16,19 @@ namespace SemesterProjekt.GUI
         {
             InitializeComponent();
         }
-
+        /// <summary>
+        /// Convert textboxes to Models.Customer(constructor) and parse to service layer
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
         private void BTN_CreateCustomer_Click(object sender, EventArgs e)
         {
+            Models.Customer customer = new Models.Customer(TB_FirstName.Text,TB_SurName.Text,
+                TB_PhoneNr.Text,TB_EMailAdress.Text,TB_Adress.Text,TB_City.Text,TB_PostalCode.Text,
+                Convert.ToDecimal(TB_Discount.Text),Convert.ToDateTime(TB_Birthday.Text),
+                Convert.ToInt32(TB_Age.Text),TB_VisionTest.Text);
 
+            Services.Customer.RegisterCustomer(customer);
         }
     }
 }
