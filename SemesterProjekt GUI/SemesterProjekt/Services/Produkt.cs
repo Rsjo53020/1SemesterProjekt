2c88f04cbab2d903eb1813ae39d5f61763c7ade8
RS\ronni <rsjo53020@edu.ucl.dk>
2023-05-31 22:10:03 +0200

    new metode

--- SemesterProjekt GUI/SemesterProjekt/Services/Produkt.cs
+++ SemesterProjekt GUI/SemesterProjekt/Services/Produkt.cs
@@ -1,7 +1,9 @@
 ﻿using SemesterProjekt.Models;
 using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
+using System.Runtime.InteropServices.ComTypes;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -28,9 +30,44 @@ namespace SemesterProjekt.Services
             Database.Database.SqlDeleteProduct(product);
         }
 
-        public static void GetAllProductFromDataBase()
+        public static void GetAllProductFromDataBase(List<Models.Frame> stockList)
         {
+            // Sortér ordrer efter dato
+            stockList = stockList.OrderBy(Frame => Frame.EAN).ToList();
+
+            Console.WriteLine("Alle produkter fra databasen i sorteret rækkefølge");
             
+            foreach (Frame frames in stockList)
+            {
+
+                Console.WriteLine($"{frames.EAN}   {frames.Kind}  ");
+            }
+
+            // Skriv til tekstfil
+            string filePath = "LagerStatus.txt";
+            using (StreamWriter writer = new StreamWriter(filePath))
+            {
+                writer.Write("Alle produkter fra databasen i sorteret rækkefølge");
+                writer.WriteLine();
+                writer.WriteLine("Kundenummer           Navn                     Dato        Køb");
+                writer.WriteLine();
+
+                foreach (Frame frames in stockList)
+                {
+                    writer.WriteLine($"{frames.EAN}   {frames.Kind}  ");
+                }
+
+                writer.WriteLine("-------------------------------------------------------------------------------");
+                writer.WriteLine();
+            }
+
+            Console.WriteLine($"Udskriften er gemt i filen: {filePath}");
         }
+
+
+
+
+    }
+}
     }
 }
