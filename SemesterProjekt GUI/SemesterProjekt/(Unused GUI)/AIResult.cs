0022723eec8d6a893ce017ddb10073ce223fa244
CHRISTOFFER\chris <chsk52994@edu.ucl.dk>
2023-06-01 14:54:34 +0200

    form

---
+++ SemesterProjekt GUI/SemesterProjekt/(Unused GUI)/AIResult.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace SemesterProjekt._Unused_GUI_
+{
+    public partial class AIResult : Form
+    {
+        public AIResult()
+        {
+            InitializeComponent();
+        }
+    }
+}
