b2b902f9104e04f67dbddeb75aca663ce4ac0376
CHRISTOFFER\chris <chsk52994@edu.ucl.dk>
2023-06-01 14:51:59 +0200

    spørgeskema

--- SemesterProjekt GUI/SemesterProjekt/(Unused GUI)/Spørgeskema.Designer.cs
+++ SemesterProjekt GUI/SemesterProjekt/(Unused GUI)/Spørgeskema.Designer.cs
@@ -54,172 +54,200 @@
             this.label1.ForeColor = System.Drawing.Color.White;
             this.label1.Location = new System.Drawing.Point(427, 47);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(290, 52);
+            this.label1.Size = new System.Drawing.Size(229, 52);
             this.label1.TabIndex = 3;
-            this.label1.Text = "SYNSPUNKT";
+            this.label1.Text = "KUNDE AI";
             // 
             // label10
             // 
             this.label10.AutoSize = true;
             this.label10.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F);
             this.label10.ForeColor = System.Drawing.Color.Gainsboro;
-            this.label10.Location = new System.Drawing.Point(212, 521);
+            this.label10.Location = new System.Drawing.Point(43, 493);
             this.label10.Name = "label10";
-            this.label10.Size = new System.Drawing.Size(119, 37);
+            this.label10.Size = new System.Drawing.Size(411, 37);
             this.label10.TabIndex = 69;
-            this.label10.Text = "Alder : ";
+            this.label10.Text = "Hvordan skal de anvendes?";
             // 
             // label7
             // 
             this.label7.AutoSize = true;
             this.label7.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F);
             this.label7.ForeColor = System.Drawing.Color.Gainsboro;
-            this.label7.Location = new System.Drawing.Point(212, 465);
+            this.label7.Location = new System.Drawing.Point(43, 635);
             this.label7.Name = "label7";
-            this.label7.Size = new System.Drawing.Size(143, 37);
+            this.label7.Size = new System.Drawing.Size(433, 37);
             this.label7.TabIndex = 67;
-            this.label7.Text = "PostNr : ";
+            this.label7.Text = "Hvilken farve foretrækker de?";
             // 
             // label6
             // 
             this.label6.AutoSize = true;
             this.label6.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F);
             this.label6.ForeColor = System.Drawing.Color.Gainsboro;
-            this.label6.Location = new System.Drawing.Point(212, 406);
+            this.label6.Location = new System.Drawing.Point(43, 564);
             this.label6.Name = "label6";
-            this.label6.Size = new System.Drawing.Size(71, 37);
+            this.label6.Size = new System.Drawing.Size(318, 37);
             this.label6.TabIndex = 65;
-            this.label6.Text = "By :";
+            this.label6.Text = "Hvilken stil søger de?";
             // 
             // label5
             // 
             this.label5.AutoSize = true;
             this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F);
             this.label5.ForeColor = System.Drawing.Color.Gainsboro;
-            this.label5.Location = new System.Drawing.Point(212, 354);
+            this.label5.Location = new System.Drawing.Point(43, 422);
             this.label5.Name = "label5";
-            this.label5.Size = new System.Drawing.Size(152, 37);
+            this.label5.Size = new System.Drawing.Size(405, 37);
             this.label5.TabIndex = 63;
-            this.label5.Text = "Adresse :";
+            this.label5.Text = "Hvilken brilletype søger de?";
             // 
             // label4
             // 
             this.label4.AutoSize = true;
             this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F);
             this.label4.ForeColor = System.Drawing.Color.Gainsboro;
-            this.label4.Location = new System.Drawing.Point(212, 298);
+            this.label4.Location = new System.Drawing.Point(43, 351);
             this.label4.Name = "label4";
-            this.label4.Size = new System.Drawing.Size(124, 37);
+            this.label4.Size = new System.Drawing.Size(417, 37);
             this.label4.TabIndex = 61;
-            this.label4.Text = "E-Mail :";
+            this.label4.Text = "Hvad er deres brille bredde?";
             // 
             // label3
             // 
             this.label3.AutoSize = true;
             this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F);
             this.label3.ForeColor = System.Drawing.Color.Gainsboro;
-            this.label3.Location = new System.Drawing.Point(212, 246);
+            this.label3.Location = new System.Drawing.Point(43, 278);
             this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(135, 37);
+            this.label3.Size = new System.Drawing.Size(425, 37);
             this.label3.TabIndex = 59;
-            this.label3.Text = "Tlf +45 :";
+            this.label3.Text = "Hvad er deres brille længde?";
             // 
             // Label2
             // 
             this.Label2.AutoSize = true;
             this.Label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F);
             this.Label2.ForeColor = System.Drawing.Color.Gainsboro;
-            this.Label2.Location = new System.Drawing.Point(212, 191);
+            this.Label2.Location = new System.Drawing.Point(43, 207);
             this.Label2.Name = "Label2";
-            this.Label2.Size = new System.Drawing.Size(171, 37);
+            this.Label2.Size = new System.Drawing.Size(313, 37);
             this.Label2.TabIndex = 57;
-            this.Label2.Text = "Efternavn :";
+            this.Label2.Text = "Hvad er deres alder?";
             // 
             // label12
             // 
             this.label12.AutoSize = true;
             this.label12.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F);
             this.label12.ForeColor = System.Drawing.Color.Gainsboro;
-            this.label12.Location = new System.Drawing.Point(212, 136);
+            this.label12.Location = new System.Drawing.Point(43, 136);
             this.label12.Name = "label12";
-            this.label12.Size = new System.Drawing.Size(153, 37);
+            this.label12.Size = new System.Drawing.Size(375, 37);
             this.label12.TabIndex = 55;
-            this.label12.Text = "Fornavn :";
+            this.label12.Text = "Hvor ønsker de at starte?";
             // 
             // comboBox1
             // 
             this.comboBox1.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.comboBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F);
             this.comboBox1.FormattingEnabled = true;
             this.comboBox1.Items.AddRange(new object[] {
             "Herrer",
             "Damer",
             "Børn"});
-            this.comboBox1.Location = new System.Drawing.Point(665, 136);
+            this.comboBox1.Location = new System.Drawing.Point(484, 128);
             this.comboBox1.Name = "comboBox1";
-            this.comboBox1.Size = new System.Drawing.Size(121, 28);
+            this.comboBox1.Size = new System.Drawing.Size(518, 45);
             this.comboBox1.TabIndex = 70;
             // 
             // comboBox2
             // 
             this.comboBox2.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.comboBox2.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F);
             this.comboBox2.FormattingEnabled = true;
-            this.comboBox2.Location = new System.Drawing.Point(665, 267);
+            this.comboBox2.Location = new System.Drawing.Point(484, 343);
             this.comboBox2.Name = "comboBox2";
-            this.comboBox2.Size = new System.Drawing.Size(121, 28);
+            this.comboBox2.Size = new System.Drawing.Size(518, 45);
             this.comboBox2.TabIndex = 71;
             // 
             // comboBox3
             // 
             this.comboBox3.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.comboBox3.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F);
             this.comboBox3.FormattingEnabled = true;
-            this.comboBox3.Location = new System.Drawing.Point(665, 223);
+            this.comboBox3.Location = new System.Drawing.Point(484, 270);
             this.comboBox3.Name = "comboBox3";
-            this.comboBox3.Size = new System.Drawing.Size(121, 28);
+            this.comboBox3.Size = new System.Drawing.Size(518, 45);
             this.comboBox3.TabIndex = 72;
             // 
             // comboBox4
             // 
             this.comboBox4.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.comboBox4.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F);
             this.comboBox4.FormattingEnabled = true;
-            this.comboBox4.Location = new System.Drawing.Point(665, 180);
+            this.comboBox4.Location = new System.Drawing.Point(484, 199);
             this.comboBox4.Name = "comboBox4";
-            this.comboBox4.Size = new System.Drawing.Size(121, 28);
+            this.comboBox4.Size = new System.Drawing.Size(518, 45);
             this.comboBox4.TabIndex = 73;
             // 
             // comboBox5
             // 
             this.comboBox5.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.comboBox5.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F);
             this.comboBox5.FormattingEnabled = true;
-            this.comboBox5.Location = new System.Drawing.Point(665, 374);
+            this.comboBox5.Items.AddRange(new object[] {
+            "Klassisk",
+            "Moderne",
+            "Sporty"});
+            this.comboBox5.Location = new System.Drawing.Point(484, 556);
             this.comboBox5.Name = "comboBox5";
-            this.comboBox5.Size = new System.Drawing.Size(121, 28);
+            this.comboBox5.Size = new System.Drawing.Size(518, 45);
             this.comboBox5.TabIndex = 77;
             // 
             // comboBox6
             // 
             this.comboBox6.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.comboBox6.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F);
             this.comboBox6.FormattingEnabled = true;
-            this.comboBox6.Location = new System.Drawing.Point(665, 417);
+            this.comboBox6.Items.AddRange(new object[] {
+            "Sort",
+            "Gul",
+            "Orange",
+            "Rød",
+            "Blå",
+            "Grøn",
+            "Brun"});
+            this.comboBox6.Location = new System.Drawing.Point(484, 627);
             this.comboBox6.Name = "comboBox6";
-            this.comboBox6.Size = new System.Drawing.Size(121, 28);
+            this.comboBox6.Size = new System.Drawing.Size(518, 45);
             this.comboBox6.TabIndex = 76;
             // 
             // comboBox7
             // 
             this.comboBox7.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.comboBox7.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F);
             this.comboBox7.FormattingEnabled = true;
-            this.comboBox7.Location = new System.Drawing.Point(665, 461);
+            this.comboBox7.Items.AddRange(new object[] {
+            "Hverdagsbrug",
+            "Sport"});
+            this.comboBox7.Location = new System.Drawing.Point(484, 485);
             this.comboBox7.Name = "comboBox7";
-            this.comboBox7.Size = new System.Drawing.Size(121, 28);
+            this.comboBox7.Size = new System.Drawing.Size(518, 45);
             this.comboBox7.TabIndex = 75;
             // 
             // comboBox8
             // 
             this.comboBox8.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.comboBox8.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F);
             this.comboBox8.FormattingEnabled = true;
-            this.comboBox8.Location = new System.Drawing.Point(665, 330);
+            this.comboBox8.Items.AddRange(new object[] {
+            "Almindelige briller",
+            "Solbriller",
+            "Læsebriller",
+            "Skærmbriller"});
+            this.comboBox8.Location = new System.Drawing.Point(484, 414);
             this.comboBox8.Name = "comboBox8";
-            this.comboBox8.Size = new System.Drawing.Size(121, 28);
+            this.comboBox8.Size = new System.Drawing.Size(518, 45);
             this.comboBox8.TabIndex = 74;
             // 
             // Spørgeskema
